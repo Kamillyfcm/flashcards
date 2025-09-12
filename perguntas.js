ab7f06ed9108e5979ab328a02467369f53e3a1c7
Kamillyfcm <kamilly.menin@escola.pr.gov.br>
2025-09-12 10:22:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Filmes',
+    'Um garoto quer namorar uma menina, mas precisa antes arrumar um pretendente para a irmã dela, que odeia regras e romances. Que filme é esse?',
+    '10 coisas que eu odeio em você'
+)
+
+criaCartao(
+    'Filmes',
+    'Esse filme é sobre uma mulher que, após dar à luz seu terceiro filho, começa a enfrentar uma depressão pós-parto enquanto lida com eventos misteriosos, sendo acusada injustamente pela comunidade e pelo próprio marido?',
+    'Uma Familia Feliz'
+)
+
+criaCartao(
+    'Filmes',
+    'Esse filme é sobre uma mulher que trabalha escrevendo cartas para analfabetos na Central do Brasil e, ao conhecer um menino perdido, embarca em uma jornada de autodescoberta e redenção?',
+    'Central do Brasil'
+)
