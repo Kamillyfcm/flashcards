8112103d7db900b3042a9f4566cbf33f2f0c54b0
Kamillyfcm <kamilly.menin@escola.pr.gov.br>
2025-09-19 09:23:33 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,12 +1,18 @@
 criaCartao(
     'Filmes',
+    'Uma jornalista decide escrever um artigo sobre como afastar um homem em poucos dias, mas acaba se apaixonando de verdade. Que filme é esse?',
+    'Como perder um homem em 10 dias'
+)
+
+criaCartao(
+    'Filmes',
     'Um garoto quer namorar uma menina, mas precisa antes arrumar um pretendente para a irmã dela, que odeia regras e romances. Que filme é esse?',
     '10 coisas que eu odeio em você'
 )
 
 criaCartao(
     'Filmes',
-    'Esse filme é sobre uma mulher que, após dar à luz seu terceiro filho, começa a enfrentar uma depressão pós-parto enquanto lida com eventos misteriosos, sendo acusada injustamente pela comunidade e pelo próprio marido?',
+    'O filme é sobre uma mulher que, após dar à luz seu terceiro filho, começa a enfrentar eventos misteriosos, sendo acusada pelo próprio marido',
     'Uma Familia Feliz'
 )
 
@@ -15,3 +21,4 @@ criaCartao(
     'Esse filme é sobre uma mulher que trabalha escrevendo cartas para analfabetos na Central do Brasil e, ao conhecer um menino perdido, embarca em uma jornada de autodescoberta e redenção?',
     'Central do Brasil'
 )
+
